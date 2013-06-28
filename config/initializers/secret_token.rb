689078ae76c7199d7645ab8f4976157995b0ecf0
Troy Shu <tmshu1@gmail.com>
2013-06-27 16:59:24 -1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeightPhotoJournal::Application.config.secret_token = '276d9b049a1a08bd8be299e02380060ede2c78b7580934f901fceb8f61fad468f619a63a0a122df5da6bc0df54041bb4d5d184cb48f0551db58cc68e09f76789'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WeightPhotoJournal::Application.config.session_store :cookie_store, key: '_weight_photo_journal_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WeightPhotoJournal::Application.config.session_store :active_record_store
